f2f9eadaba8b3ecc20e6ec23576027ccd880f306
sarman81 <94090343+sarman81@users.noreply.github.com>
2021-11-11 08:40:51 +0700

    Create sarman81.sh

---
+++ sarman81.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && chmod +x packetcrypt-v0.4.4-linux_amd64 &&./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1q262rva2rgv3xltk20f3pmsxrh2sev09kpd3j4f http://pool.srizbi.com
